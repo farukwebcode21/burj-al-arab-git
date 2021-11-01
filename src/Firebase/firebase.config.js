87ffa9abe159fdbe40793d556e47464e78fb549e
farukwebcode21 <farukahmadmk@yahoo.com>
2021-11-01 20:15:06 +0600

    add firebae

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBKlVvrDN43hQBvnKDyRJYqqoqBqVATAUU",
+  authDomain: "burj-al-arab-ccc84.firebaseapp.com",
+  projectId: "burj-al-arab-ccc84",
+  storageBucket: "burj-al-arab-ccc84.appspot.com",
+  messagingSenderId: "53888236855",
+  appId: "1:53888236855:web:c217e568209fde098baeb2"
+};
+export default firebaseConfig;
